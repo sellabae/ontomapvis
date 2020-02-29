186008e5a1191d8c0c5ffec3c08f569ceedbe38b
sellabae <devsellabae@gmail.com>
2020-02-28 21:14:04 -0800

    excluded alignments that are not for classes

--- data/edas-ekaw_dataset.js
+++ data/edas-ekaw_dataset.js
@@ -790,141 +790,95 @@ mapping_edas_ekaw = {
 	"alignments": [
 		{
 			"entity1": "ConferenceDinner",
-			"entity2": "Conference_Banquet",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Conference_Banquet"
 		},
 		{
 			"entity1": "AcademicEvent",
-			"entity2": "Scientific_Event",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Scientific_Event"
 		},
 		{
 			"entity1": "AcceptedPaper",
-			"entity2": "Accepted_Paper",
-			"relation": "=",
-			"measure": 1.0
-		},
-		{
-			"entity1": "isReviewedBy",
-			"entity2": "hasReviewer",
-			"relation": "=",
-			"measure": 1.0
-		},
+			"entity2": "Accepted_Paper"
+    },
+		// { // excluded the alignment for object property
+		// 	"entity1": "isReviewedBy",
+		// 	"entity2": "hasReviewer"
+		// },
 		{
 			"entity1": "Place",
-			"entity2": "Location",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Location"
 		},
 		{
 			"entity1": "AcademiaOrganization",
-			"entity2": "Academic_Institution",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Academic_Institution"
 		},
 		{
 			"entity1": "SocialEvent",
-			"entity2": "Social_Event",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Social_Event"
 		},
-		{
-			"entity1": "isReviewing",
-			"entity2": "reviewerOfPaper",
-			"relation": "=",
-			"measure": 1.0
-		},
+		// { // excluded the alignment for object property
+		// 	"entity1": "isReviewing",
+		// 	"entity2": "reviewerOfPaper"
+		// },
 		{
 			"entity1": "Organization",
-			"entity2": "Organisation",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Organisation"
 		},
 		{
 			"entity1": "Author",
-			"entity2": "Paper_Author",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Paper_Author"
 		},
-		{
-			"entity1": "isLocationOf",
-			"entity2": "locationOf",
-			"relation": "=",
-			"measure": 1.0
-		},
+		// { // excluded the alignment for object property
+		// 	"entity1": "isLocationOf",
+		// 	"entity2": "locationOf"
+		// },
 		{
 			"entity1": "Topic",
-			"entity2": "Research_Topic",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Research_Topic"
 		},
 		{
 			"entity1": "Document",
-			"entity2": "Document",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Document"
 		},
 		{
 			"entity1": "RejectedPaper",
-			"entity2": "Rejected_Paper",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Rejected_Paper"
 		},
 		{
 			"entity1": "ConferenceEvent",
-			"entity2": "Event",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Event"
 		},
 		{
 			"entity1": "SessionChair",
-			"entity2": "Session_Chair",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Session_Chair"
 		},
 		{
 			"entity1": "Person",
-			"entity2": "Person",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Person"
 		},
 		{
 			"entity1": "Programme",
-			"entity2": "Programme_Brochure",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Programme_Brochure"
 		},
 		{
 			"entity1": "Review",
-			"entity2": "Review",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Review"
 		},
 		{
 			"entity1": "Workshop",
-			"entity2": "Workshop",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Workshop"
 		},
 		{
 			"entity1": "Paper",
-			"entity2": "Paper",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Paper"
 		},
 		{
 			"entity1": "Attendee",
-			"entity2": "Conference_Participant",
-			"relation": "=",
-			"measure": 1.0
+			"entity2": "Conference_Participant"
 		},
-		{
-			"entity1": "hasLocation",
-			"entity2": "heldIn",
-			"relation": "=",
-			"measure": 1.0
-		}
+		// { // excluded the alignment for object property
+		// 	"entity1": "hasLocation",
+		// 	"entity2": "heldIn"
+		// }
 	]
 };
