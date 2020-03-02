480885182417b7b14e40bedc0598ef086d82687c
sellabae <devsellabae@gmail.com>
2020-03-02 04:20:10 -0800

    get separate file ready

---
+++ baseline.js
@@ -0,0 +1,7 @@
+
+baselineMappingView = function (ont1root, ont2root, alignments) {
+    console.log(`baselineMappingView(ont1root, ont2root, alignments)`);
+
+    //Creates a detached g to return
+    const g = d3.create("svg:g");
+};
---
+++ matrix.js
@@ -0,0 +1,7 @@
+
+matrixMappingView = function (ont1root, ont2root, alignments) {
+    console.log(`matrixMappingView(ont1root, ont2root, alignments)`);
+
+    //Creates a detached g to return
+    const g = d3.create("svg:g");
+};
