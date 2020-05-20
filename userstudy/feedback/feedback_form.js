2ae9fd5f25b15572f26b2fb31772986171d9a211
sellabae <devsellabae@gmail.com>
2020-05-19 19:40:42 -0700

    update scale based form generator

--- userstudy/feedback/feedback_form.js
+++ userstudy/feedback/feedback_form.js
@@ -1,99 +1,100 @@
-const FormTypeNum = {
+const FormCode = {
     workload: 0,
     usability: 1,
     reaction: 2
-};
-const FormType = {
-    workload: "tlx",
-    usability: "sus",
-    reaction: "reaction"
 };
 
 window.addEventListener('load', function() {
     console.log("window loaded.");
 
-    
     var data = formdata.tlx;
-    showFormHead(data);
-    drawScaleForm(data);
-    // var selected = FormTypeNum.workload; //0,1,2
-    // switch(selected) {
-    //     case FormTypeNum.workload:
-    //     case FormType.usability:
-    //         drawQuestionForm(selectedForm);
-    //         break;
-    //     case FormType.reaction:
-    //         drawReactionForm();
-    //         break;
-    //     default:
-    //         console.log("invalid formtype is selected.");
-    // }
+    generateForm(data);
 });
 
-function showFormHead(data) {
+/**
+ * Generate feedback form
+ * @param {object} data formdata object
+ */
+function generateForm(data) {
+    console.log('generating form...');
+
+    //Show form description
     $("#form-title").text(data.title);
     $("#form-message").text(data.message);
+
+    //Add hidden id/formtype info for form submit
+    var pid = "E9R7A001"; //some 8-digit random generated id
+    $('#form-identification').append(`<input type="text" id="pId" name="pId" value="${pid}" readonly>`);
+    $('#form-identification').append(`<input type="text" id="formtype" name="type" value="${data.type}" readonly>`);
+    
+    //Draw the form on html page
+    console.log('formtype: ' + data.formcode);
+    switch(data.formcode) {
+        case FormCode.workload:
+        case FormCode.usability:
+            drawScaleForm(data);
+            break;
+        case FormCode.reaction:
+            drawReactionForm(data);
+            break;
+        default:
+            console.log("invalid formtype is selected.");
+    }
 }
 
 /**
- * Draw scale based questionnaire type of form.
+ * Draw scale based questionnaire type of form to html.
  * @param {object} formdata. either workload tlx or usability sus
  */
 function drawScaleForm(data) {
     console.log("drawScaleForm()");
     if (data.itmes == "undefined") return;
     
-    data.items.forEach((d,i) => {
-        createQuestionItem(d,i).appendTo('form');
-    });
-}
+    data.items.forEach((item,i) => {
+        idx = i+1;
+        //question item
+        var itemDiv = $('<div/>', {
+            id: 'item' + idx,
+            class: 'itemdiv'
+        });
+        //question text
+        text = idx + ". " + item.question;
+        itemDiv.append($('<p/>', {class: 'qtext', html: text}));
+        //question subtext
+        if (item.subtext != "undefined") {
+            itemDiv.append($('<p/>', {class: 'qtext-sub', html: item.subtext}));
+        }
+        //radio group
+        itemDiv.append(createRadioGroup("a"+idx, data.scale));
 
-function createQuestionItem(item, i) {
-    idx = i+1;
-    //question item
-    var itemDiv = $('<div/>', {
-        id: 'item' + idx,
-        class: 'itemdiv'
+        $('#form-content').append(itemDiv);
     });
-    //question text
-    text = idx + ". " + item.question;
-    itemDiv.append($('<p/>', {class: 'qtext', html: text}));
-    //question subtext
-    if (item.subtext != "undefined") {
-        itemDiv.append($('<p/>', {class: 'qtext-sub', html: item.subtext}));
-    }
-    //radio group
-    itemDiv.append(createRadioGroup(5, 'very low', 'very high'));
-
-    return itemDiv;
 }
 
 /**
  * Create a radio group
- * @param {number} pointCount 
- * @param {string} firstWord 
- * @param {string} lastWord 
+ * @param {string} inputName name of the radio group
+ * @param {object} scale {count, fisrt, last}
  */
-function createRadioGroup(pointCount, firstWord, lastWord) {
+function createRadioGroup(inputName, scale) {
     var answerDiv = $('<div class="answerdiv text-center">');
-    var radioDiv = $('<div class="form-check form-check-inline" role="radiogroup">');
-    radioDiv.append($('<span>'+ firstWord +'</span>'));
-    for (var i=1; i<=pointCount; i++) {
-        radioDiv.append($('<input/>', {
+    answerDiv.append($('<label>'+ scale.first +'</label>'));
+    var radiogroup = $('<div class="form-check form-check-inline" role="radiogroup">');
+    for (var i=1; i<=scale.count; i++) {
+        var radio = $('<div class="radiodiv"></div>');
+        radio.append($(`<label class="form-check-label">${i}</label>`));
+        radio.append($('<input/>', {
             class: "form-check-input",
             type: "radio",
-            name: "inlineRadioOptions",
+            name: inputName,
             id: "inlineRadio" + i,
-            value: i
+            value: i,
+            required: "required"
         }));
+        radiogroup.append(radio);
     }
-    radioDiv.append($('<span>'+ lastWord +'</span>'));
-    answerDiv.append(radioDiv);
+    answerDiv.append(radiogroup);
+    answerDiv.append($('<label>'+ scale.last +'</label>'));
     return answerDiv;
 }
 
-
-
-function drawReactionCardForm(formdata) {
-
-}
--- userstudy/feedback/formdata.js
+++ userstudy/feedback/formdata.js
@@ -1,8 +1,15 @@
 const formdata = {
     "tlx": {
+        "formcode": 0,
+        "type": "tlx",
         "title": "Workload",
         "desc": "NASA-TLX Workload",
         "message": "Please select a point on the scale of 1 to 5 that best describes your experience during the task you've just completed. There is no right or wrong answer.",
+        "scale": {
+            "count": 5,
+            "first": "very low",
+            "last": "very high"
+        },
         "items": [
             {
                 "question": "How mentally demanding was the task?",
@@ -26,23 +33,43 @@ const formdata = {
         ]
     },
     "sus": {
+        "formcode": 1,
+        "type": "sus",
         "title": "Usability",
         "desc": "System Usability Scale(SUS)",
         "message": "Please select a point on the scale of 1 to 5 that best describes how much you agree/disagree with each statement below about the visualization you've just used to complete the task. There is no right or wrong answer.",
+        "scale": {
+            "count": 5,
+            "first": "strongly disagree",
+            "last": "strongly agree"
+        },
         "items": [
-            "I think that I would like to use this visualization frequently (when completing similar tasks).",
-            "I found the visualization unnecessarily complex.",
-            "I thought the visualization was easy to use.",
-            "I think I would need the support of a technical person to be able to use this visualization.",
-            "I found the various functions in this visualization were well integrated.",
-            "I thought this visualization was too inconsistent.",
-            "I would imagine that most people would learn to use this visualization very quickly.",
-            "I found the visualization very cumbersome to use.",
-            "I felt very confident using the visualization.",
-            "I needed to learn a lot of things before I could get going with this visualization."
+            {
+                "question": "I think that I would like to use this visualization frequently (when completing similar tasks).",
+            },{
+                "question": "I found the visualization unnecessarily complex.",
+            },{
+                "question": "I thought the visualization was easy to use.",
+            },{
+                "question": "I think I would need the support of a technical person to be able to use this visualization.",
+            },{
+                "question": "I found the various functions in this visualization were well integrated.",
+            },{
+                "question": "I thought this visualization was too inconsistent.",
+            },{
+                "question": "I would imagine that most people would learn to use this visualization very quickly.",
+            },{
+                "question": "I found the visualization very cumbersome to use.",
+            },{
+                "question": "I felt very confident using the visualization.",
+            },{
+                "question": "I needed to learn a lot of things before I could get going with this visualization."
+            }
         ]
     },
     "reaction": {
+        "formcode": 2,
+        "type": "reaction",
         "title": "Reaction Cards",
         "desc": "Microsoft Product Desirability Reaction Cards (64 out of 118 words)",
         "message": "Please select the top five words that best describe your opinion on the visualization you've just used to complete the task. There is no right or wrong answer.",
@@ -111,6 +138,7 @@ const formdata = {
             "Unpredictable",
             "Unrefined",
             "Useful"
-        ]
+        ],
+        "message1": "Please explain your choices of these words."
     }
-}
+};
